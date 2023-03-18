e1316bf3d9748454b2de9bb6ae58385ba83d6049
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-03-18 17:28:20 -0400

    remove main

--- main.go
+++ main.go
@@ -1,48 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-	"strings"
-)
-
-const callbackDivider = "-"
-
 func main() {
-	some := "a----"
-
-	fmt.Println(parseCallback(some))
-}
-func parseCallback(callback string) ([]string, error) {
-	if callback == "" {
-		return nil, fmt.Errorf("empty callback")
-	}
-	if strings.Contains(callback, " ") {
-		return nil, fmt.Errorf("invalid callback")
-	}
-	callbackElements := strings.Split(callback, callbackDivider)
-	var resp = make([]string, 0, len(callbackElements))
-	for i := range callbackElements {
-		symbol, err := extractSymbolFromElem(callbackElements[i])
-		if err != nil {
-			return nil, err
-		}
-		resp = append(resp, symbol)
-	}
-	return resp, nil
-}
-
-func extractSymbolFromElem(in string) (string, error) {
-	valid, err := checkCallbackElement(in)
-	if err != nil {
-		return "", err
-	}
-	if !valid {
-		return "", fmt.Errorf("element is not valid")
-	}
-	return string(in[0]), nil
-}
-
-func checkCallbackElement(element string) (bool, error) {
-	return regexp.MatchString(`^[a-z](\(.+\))?$`, element)
 }
