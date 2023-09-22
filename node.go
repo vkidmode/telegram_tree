398399e72c66df7cbde9a1b1ce954132f0e06b5e
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-09-22 18:35:30 +0200

    fix

--- node.go
+++ node.go
@@ -147,7 +147,7 @@ func (n *node) fillNextNodes(ctx context.Context, meta Meta) (err error) {
 	if n.processor != nil {
 		processorNodes, err = n.processor(ctx, meta)
 		if err != nil {
-			return fmt.Errorf("processing")
+			return fmt.Errorf("processing %w", err)
 		}
 	}
 
